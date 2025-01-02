8ca38f93937c39363afd94c3340b702cbb4d9afc
Alberto F. Merchán <alberto.fernmer@gmail.com>
2025-01-02 17:48:15 +0100

    Vision 3D

---
+++ [V3D] Vision 3D/Parte 2. Calibración de Cámaras/chessboard_calibration/3.1_cameracalibration/CMakeFiles/drawAxis.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/z42ferma/Desktop/Alberto/chessboard_calibration/3.1_cameracalibration/drawAxis.cpp" "CMakeFiles/drawAxis.dir/drawAxis.cpp.o" "gcc" "CMakeFiles/drawAxis.dir/drawAxis.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
