5cfe44250e754aebd7309173ff05e7d64697cd10
Alberto F. Merchán <alberto.fernmer@gmail.com>
2025-01-02 17:48:15 +0100

    Vision 3D

---
+++ [V3D] Vision 3D/Parte 2. Calibración de Cámaras/chessboard_calibration/3.1_cameracalibration/build/CMakeFiles/drawAxis.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/drawAxis.dir/drawAxis.cpp.o"
+  "CMakeFiles/drawAxis.dir/drawAxis.cpp.o.d"
+  "drawAxis"
+  "drawAxis.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/drawAxis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
